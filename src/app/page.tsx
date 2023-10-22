011d9502b0044fb9220d5bddabf59b0472cb4c67
johnny <desarrollo3@pcnub.com>
2023-10-22 16:54:01 -0500

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { AuthButton } from './components/auth-button'
 
-export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: post } = await supabase.from('post').select('*');
-
+export default async function Home () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: post } = await supabase.from('post').select('*')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         Hola Twitter
+        <AuthButton />
         <pre>
-          { JSON.stringify( post, null, 2)}
+          { JSON.stringify(post, null, 2)}
         </pre>
     </main>
   )
